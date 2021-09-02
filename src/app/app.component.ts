90e38ff668e9a26811ead3f2cbb4e91e0213c95e
Luis Cabrera Benito <parzibyte@gmail.com>
2021-09-02 15:11:02 -0500

    Input para tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-tareas-angular';
+  tarea = ""
+  guardarTarea(){
+    console.log("Se guarda la tarea " + this.tarea)
+  }
 }
